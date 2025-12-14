b92d78b210d2c80df533fd1f731cb6b454c02664
piziwoo <79001804a@gmail.com>
2025-12-14 19:37:26 +0800

    Update flyclash.js

--- flyclash.js
+++ flyclash.js
@@ -15,7 +15,6 @@ external-controller-cors:
   allow-origins:
     - "*"
   allow-private-network: true
-
 dns:
   enable: true
   ipv6: false
@@ -47,9 +46,7 @@ dns:
   nameserver:
     - https://dns.google/dns-query
     - https://cloudflare-dns.com/dns-query
-
 proxy-groups:
-
   # 主开关，手动选择地区
   - name: PROXY
     icon: "https://cdn.jsdelivr.net/gh/GitMetaio/Surfing@rm/Home/icon/All.svg"
@@ -62,7 +59,6 @@ proxy-groups:
       - 日本
       - 美国
       - 其他
-
   # 以下各地区组：自动测速 + 自动选最佳节点
   - name: 香港
     icon: "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Hong_Kong.png"
@@ -72,7 +68,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     filter: (?i)港|HK|hk|Hong Kong|HongKong|hongkong
-
   - name: 新加坡
     icon: "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Singapore.png"
     type: url-test
@@ -81,7 +76,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     filter: (?i)新加坡|坡|狮城|SG|Singapore
-
   - name: 台湾
     icon: "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Taiwan.png"
     type: url-test
@@ -90,7 +84,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     filter: (?i)台|新北|彰化|TW|Taiwan
-
   - name: 日本
     icon: "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Japan.png"
     type: url-test
@@ -99,7 +92,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     filter: (?i)日本|川日|东京|大阪|泉日|埼玉|沪日|深日|[^-]日|JP|Japan
-
   - name: 美国
     icon: "https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/United_States.png"
     type: url-test
@@ -108,7 +100,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     filter: (?i)美|波特兰|达拉斯|俄勒冈|凤凰城|费利蒙|硅谷|拉斯维加斯|洛杉矶|圣何塞|圣克拉拉|西雅图|芝加哥|US|United States|UnitedStates
-
   - name: 其他
     icon: "https://cdn.jsdelivr.net/gh/GitMetaio/Surfing@rm/Home/icon/Globe.svg"
     type: url-test
@@ -117,7 +108,6 @@ proxy-groups:
     tolerance: 50
     include-all: true
     exclude-filter: (?i)港|HK|hk|Hong Kong|HongKong|hongkong|日本|川日|东京|大阪|泉日|埼玉|沪日|深日|[^-]日|JP|Japan|美|波特兰|达拉斯|俄勒冈|凤凰城|费利蒙|硅谷|拉斯维加斯|洛杉矶|圣何塞|圣克拉拉|西雅图|芝加哥|US|United States|UnitedStates|台|新北|彰化|TW|Taiwan|新加坡|坡|狮城|SG|Singapore|灾|网易|Netease|套餐|重置|剩余|到期|订阅|群|账户|流量|有效期|时间|官网
-
 rule-anchor:
   Classical: &Classical
     {type: http, behavior: classical, format: text, interval: 86400}
@@ -127,15 +117,12 @@ rule-anchor:
     {type: http, behavior: ipcidr, format: mrs, interval: 86400}
   Domain: &Domain
     {type: http, behavior: domain, format: mrs, interval: 86400}
-
 rule-providers:
-
 #防泄漏
   WebRTC:
     <<: *Classical
     path: ./rules/WebRTC.list
     url: "https://cdn.jsdelivr.net/gh/GitMetaio/Surfing@rm/Home/rules/WebRTC.list"
-
 #私有/内网
   Private_Domain:
     <<: *Domain
@@ -145,7 +132,6 @@ rule-providers:
     <<: *IPCIDR
     path: ./rules/LAN_IP.mrs
     url: "https://github.com/GitMetaio/rule/raw/refs/heads/master/rule/Clash/Lan/Lan_OCD_IP.mrs"
-
 #代理规则
   Global_Domain:
     <<: *Domain
@@ -155,14 +141,17 @@ rule-providers:
     <<: *IPCIDR
     path: ./rules/Global_OCD_IP.mrs
     url: "https://cdn.jsdelivr.net/gh/GitMetaio/rule@master/rule/Clash/Global/Global_OCD_IP.mrs"
-
 #去广告
   AWAvenue_Ads_Rule:
     <<: *YAML
     path: ./ruleset/AWAvenue_Ads_Rule_Clash.yaml
     url: "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Clash.yaml"
 
+#手动添加
 rules:
+  - DOMAIN-SUFFIX,okx.com,PROXY
+  - DOMAIN-SUFFIX,bilibili.com,DIRECT
+
   - DST-PORT,53,DIRECT
   - DST-PORT,853,DIRECT
   - RULE-SET,WebRTC,REJECT
